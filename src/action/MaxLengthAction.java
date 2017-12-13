f2102b410bcefc4a632fefccee7eacf7349d9217
Jokertwo <p.lastovka@seznam.cz>
2017-12-13 02:49:49 +0100

    Action of maxLength string

---
+++ src/action/MaxLengthAction.java
@@ -0,0 +1,45 @@
+package action;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.logging.Logger;
+import javax.swing.text.JTextComponent;
+
+
+public class MaxLengthAction implements KeyListener {
+
+    protected JTextComponent component;
+    protected int maxLength;
+    Logger log = Logger.getLogger(MaxLengthAction.class.getName());
+
+
+    public MaxLengthAction(JTextComponent component, int maxLength) {
+        this.component = component;
+        this.maxLength = maxLength;
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (component.getText().length() >= maxLength) {
+            component.setText(component.getText().substring(0, component.getText().length() - 1));
+
+        }
+
+    }
+
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
