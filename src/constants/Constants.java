29c72491176a945ee405d5d3a80fc27cb0f481c6
Jokertwo <p.lastovka@seznam.cz>
2017-12-13 02:50:52 +0100

    Constantc

---
+++ src/constants/Constants.java
@@ -0,0 +1,25 @@
+package constants;
+
+public class Constants {
+
+    private Constants() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static final String ALL = "1";
+    public static final String PRIVATE = "2";
+    public static final String PING = "3";
+    public static final String LOG_IN = "4";
+    public static final String LOG_OUT = "5";
+    public static final String CON_USERS = "6";
+    public static final String OK = "7";
+    public static final String ERROR = "8";
+    public static final String SHUTDOWN = "9";
+    public static final String CHAT_ALL = "All";
+    public static final String SEPARATOR = ";";
+    
+    public static final String UNDEF = "-1";
+
+    public static final int MAX_MESSAGE_LENGTH = 1000;
+
+}
