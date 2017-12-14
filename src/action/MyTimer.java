38b8f18690d9513bfc02620eab42b2514cbb342d
Jokertwo <p.lastovka@seznam.cz>
2017-12-14 18:48:17 +0100

    Timer
    Po urcite dobe schova label

---
+++ src/action/MyTimer.java
@@ -0,0 +1,35 @@
+package action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+
+
+public class MyTimer {
+    private static final int DEFAULT_TIME = 4000;
+
+
+    public MyTimer(JLabel infoLabel) {
+        runTimer(infoLabel, DEFAULT_TIME);
+    }
+
+
+    public MyTimer(JLabel infoLabel, int time) {
+        runTimer(infoLabel, time);
+    }
+
+
+    private void runTimer(JLabel infoLabel, int time) {
+        Timer t = new Timer(time, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                infoLabel.setText(" ");
+            }
+        });
+        t.setRepeats(false);
+        t.start();
+    }
+
+}
