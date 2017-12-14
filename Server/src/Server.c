264838701f6caf156caba7e3ad6c1b496ebf1d54
Jokertwo <p.lastovka@seznam.cz>
2017-12-14 18:54:39 +0100

    read-write lock

--- Server/src/Server.c
+++ Server/src/Server.c
@@ -21,8 +21,15 @@ typedef struct {
 
 Client clients[MAX_USER];
 pthread_mutex_t mutex;
+pthread_rwlock_t lock;
 
 int main(int argc, char *argv[]) {
+
+	if (pthread_rwlock_init(&lock, NULL) != 0) {
+		error_log("Cant inicialize rw_lock server cant start.\n");
+		exit(2);
+	}
+
 	(void) signal(SIGINT, shotdownServer);
 	int socket_desc, client_sock, c;
 	struct sockaddr_in server, client;
@@ -96,7 +103,7 @@ void *connection_handler(void *socket_desc) {
 		size = recv(sock, buf, BUFFER_SIZE, 0);
 
 		strncpy(sendedSize, buf, sizeof(sendedSize));
-		sendedSize[sizeof(sendedSize)-1] = '\0';
+		sendedSize[sizeof(sendedSize) - 1] = '\0';
 
 		//if message is not hole, wait for left
 		while (atoi(sendedSize) > size) {
@@ -129,96 +136,103 @@ void shotdownServer(int sig) {
 	printf("Server will be shotDown. \n");
 	parseMessage(0, "9", 0);
 	(void) signal(SIGINT, SIG_DFL);
-	exit(0);
-
+	exit(1);
 }
 /**
  * parse received message
  */
 int parseMessage(int sock, char *message, int size) {
 	printf("Server received : %s", message);
-	pthread_mutex_lock(&mutex);
+
 	int i;
 	if ((i = atoi(&message[0])) != 0) {
 		switch (i) {
 
 		case 1:
+			pthread_rwlock_rdlock(&lock);
 			if (send_to_all(size, message, sock) == FALSE) {
 				error_log("Error during send message to all. \n Message : %s",message +1)
-				send_error(sock,ERROR_DEFAULT);
-				pthread_mutex_unlock(&mutex);
+				send_error(sock, ERROR_DEFAULT);
+				pthread_rwlock_unlock(&lock);
 				return FALSE;
 			}
 			send_ok(sock);
-			pthread_mutex_unlock(&mutex);
+			pthread_rwlock_unlock(&lock);
 			return TRUE;
 		case 2:
+			pthread_rwlock_rdlock(&lock);
 			if (send_private_message(message, size) == FALSE) {
 				error_log("Can't send private message : %s",message);
-				send_error(sock,ERROR_DEFAULT);
-				pthread_mutex_unlock(&mutex);
+				send_error(sock, ERROR_DEFAULT);
+				pthread_rwlock_unlock(&lock);
 				return FALSE;
 			}
 			send_ok(sock);
-			pthread_mutex_unlock(&mutex);
+			pthread_rwlock_unlock(&lock);
 			return TRUE;
 		case 3:
+			pthread_rwlock_rdlock(&lock);
 			if (send_ping(sock) == FALSE) {
 				error_log("Can't send ping to : %s \n", message +1);
-				pthread_mutex_unlock(&mutex);
+				pthread_rwlock_unlock(&lock);
 				return FALSE;
 			}
-			pthread_mutex_unlock(&mutex);
+			pthread_rwlock_unlock(&lock);
 			return TRUE;
 		case 4:
+			pthread_rwlock_wrlock(&lock);
 			if (number_of_client >= MAX_USER) {
 				send_error(sock, ERROR_MAX_USERS);
-				pthread_mutex_unlock(&mutex);
+				pthread_rwlock_unlock(&lock);
 				return FALSE;
 			}
 			if (add_new_client(sock, message, size) == FALSE) {
 				error_log("Can't create new user. User with name %s exist \n",message+1);
 				send_error(sock, ERROR_USER_EXIST);
-				pthread_mutex_unlock(&mutex);
+				pthread_rwlock_unlock(&lock);
 				return FALSE;
 			}
 			send_ok(sock);
 			informAboutConnectClient();
-			pthread_mutex_unlock(&mutex);
+			pthread_rwlock_unlock(&lock);
 			printf("Login new user : %s \n", message + 1);
 			return TRUE;
 		case 5:
+			pthread_rwlock_wrlock(&lock);
 			if (remove_client(sock, message + 1) == FALSE) {
 				error_log("Can't logout client %s because I can't find his index",message+1);
 				send_error(sock, ERROR_DEFAULT);
-				pthread_mutex_unlock(&mutex);
+				pthread_rwlock_unlock(&lock);
 				return FALSE;
 			}
 			send_ok(sock);
 			informAboutConnectClient();
-			pthread_mutex_unlock(&mutex);
+			pthread_rwlock_unlock(&lock);
 			printf("Logout user : %s \n", message + 1);
 			return LOGOUT;
 		case 6:
+			pthread_rwlock_rdlock(&lock);
 			if (send_user_list(sock) == FALSE) {
 				error_log("Can't send user list to %s", message +1);
-				pthread_mutex_unlock(&mutex);
+				pthread_rwlock_unlock(&lock);
 				return FALSE;
 			}
-			pthread_mutex_unlock(&mutex);
+			pthread_rwlock_unlock(&lock);
 			return TRUE;
 		case 9:
+			pthread_rwlock_rdlock(&lock);
 			logOutAll();
-			break;
+			pthread_rwlock_unlock(&lock);
+			return FALSE;
 		default:
+			pthread_rwlock_rdlock(&lock);
 			error_log("Can't parse message: %s",message)
 			;
-			pthread_mutex_unlock(&mutex);
+			pthread_rwlock_unlock(&lock);
 			return FALSE;
 		}
 	} else {
 		error_log("Can't parse message : %s",message);
-		pthread_mutex_unlock(&mutex);
 		return FALSE;
 	}
 	return 0;
@@ -338,14 +352,17 @@ int send_to_all(int size, char *buffer, int socket) {
  */
 int send_user_list(int socket) {
 	char *temp;
-	int length_of_logins = 0;
+	int length_of_logins = 3;
 
 	for (int i = 0; i < number_of_client; i++) {
 		if (clients[i].socket != socket) {
-			length_of_logins += strlen(clients[i].login) + 2;
+			length_of_logins += strlen(clients[i].login) + 1;
 		}
 	}
-	temp = calloc(length_of_logins + 1, sizeof(char));
+	if ((temp = malloc(length_of_logins * sizeof(char))) == NULL) {
+		return FALSE;
+	}
+	memset(temp, '\0', length_of_logins);
 	strcat(temp, "6");
 	for (int i = 0; i < number_of_client; i++) {
 		if (clients[i].socket != socket) {
@@ -383,18 +400,21 @@ int send_private_message(char *message, int size) {
 	char *to_user;
 	char *position;
 	if ((position = strchr(message, SEPARATOR)) != NULL) {
-		size_of_name = strlen(message) - strlen(position);
-		to_user = calloc(size_of_name, sizeof(char));
-		strncpy(to_user, message + 1, size_of_name);
-		printf("Send message to user : %s\n", to_user);
-		if ((index = get_client_index(to_user)) != FALSE) {
-			if (send(clients[index].socket, message, size, 0) > 0) {
-				printf("Sended message : %s", message);
-				free(to_user);
-				return TRUE;
+		size_of_name = (strlen(message) - strlen(position)) + 1;
+		if ((to_user = malloc(size_of_name * sizeof(char))) != NULL) {
+			memset(to_user, '\0', size_of_name);
+			strncpy(to_user, message + 1, size_of_name - 1);
+			printf("Send message to user : %s\n", to_user);
+			if ((index = get_client_index(to_user)) != FALSE) {
+				if (send(clients[index].socket, message, size, 0) > 0) {
+					printf("Sended message : %s", message);
+					free(to_user);
+					return TRUE;
+				}
 			}
+			free(to_user);
+			return FALSE;
 		}
-		free(to_user);
 		return FALSE;
 	}
 
